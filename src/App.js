89afd3198813b7dd5306e745dcde56bb1a8a9fdc
Hromenique Cezniowscki Leite Batista <hromenique@gmail.com>
2018-10-17 00:09:30 -0300

    após aula 02 atividade 01

--- src/App.js
+++ src/App.js
@@ -3,8 +3,21 @@ import Header from './componentes/Header'
 import Timeline from './componentes/Timeline';
 import Toast from './componentes/Toast';
 import TimelineStore from './logicas/TimelineStore';
+import {createStore} from 'redux';
 
 const timelineStore = new TimelineStore([]);
+
+//REDUCER
+function timeline(state=[], action){
+  if(action.type === 'LISTAGEM'){
+    console.log('entrou na listagem');
+    return action.fotos;
+  }
+
+  return state;
+}
+
+const store = createStore(timeline);
 
 class App extends Component {
   render() {
@@ -12,7 +25,7 @@ class App extends Component {
       <div id="root">
         <div className="main">
           <Header  store={timelineStore}/>
-          <Timeline login={this.props.params.login} store={timelineStore}/>
+          <Timeline login={this.props.params.login} store={store}/>
           <Toast />
         </div>
       </div>
--- src/componentes/Timeline.js
+++ src/componentes/Timeline.js
@@ -12,9 +12,9 @@ export default class Timeline extends Component {
     }
 
     componentWillMount() {
-        this.store.subscribe(fotos => {
-            this.setState({fotos});
-        });        
+        this.store.subscribe(() => {
+            this.setState({fotos: this.store.getState()}) 
+        });
     }
 
     componentDidMount() {
@@ -36,7 +36,11 @@ export default class Timeline extends Component {
         } else {
             urlPerfil = `http://localhost:8080/api/public/fotos/${this.login}`;
         }
-        this.store.lista(urlPerfil);        
+
+        const listaFixa = [{"urlPerfil":"https://instagram.fcgh9-1.fna.fbcdn.net/vp/faf1cd7c1d50bbf382cad0d43df15a49/5B5FF9ED/t51.2885-19/s150x150/12599387_1591433254512484_973178862_a.jpg","loginUsuario":"rafael","horario":"16/10/2018 23:28","urlFoto":"https://i.pinimg.com/564x/7f/cb/57/7fcb570520831df31cd11d25d56b896d.jpg","id":4,"likeada":false,"likers":[],"comentarios":[],"comentario":"comentario da foto 1"},{"urlPerfil":"https://instagram.fcgh9-1.fna.fbcdn.net/vp/faf1cd7c1d50bbf382cad0d43df15a49/5B5FF9ED/t51.2885-19/s150x150/12599387_1591433254512484_973178862_a.jpg","loginUsuario":"rafael","horario":"16/10/2018 23:28","urlFoto":"https://i.pinimg.com/564x/ee/f7/79/eef77989d1053d1c36789ee799e79ff1.jpg","id":5,"likeada":false,"likers":[],"comentarios":[],"comentario":"comentario da foto 2"},{"urlPerfil":"https://instagram.fcgh9-1.fna.fbcdn.net/vp/faf1cd7c1d50bbf382cad0d43df15a49/5B5FF9ED/t51.2885-19/s150x150/12599387_1591433254512484_973178862_a.jpg","loginUsuario":"rafael","horario":"16/10/2018 23:28","urlFoto":"https://i.pinimg.com/564x/7f/cb/57/7fcb570520831df31cd11d25d56b896d.jpg","id":6,"likeada":false,"likers":[],"comentarios":[],"comentario":"comentario da foto 3"}];
+        
+        this.store.dispatch({type: 'LISTAGEM', fotos: listaFixa});
+        // this.store.lista(urlPerfil);        
     }   
 
     like(fotoId){   
